ae6b8282516a0dd0b24b4be39eb0cfc5110ef6cf
Zebb Prime <zebb.prime@gmail.com>
2012-07-09 19:43:12 +0930

    Modified the s-function to do operate with no joystick.

--- src/sfun_osx_joystick.cpp
+++ src/sfun_osx_joystick.cpp
@@ -32,9 +32,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "osx_joystick.hpp"
 
 // Parameter indicies
-#define NUM_PARAMS 2
+#define NUM_PARAMS 6
 #define P_JOYID 0
 #define P_TS 1
+#define P_LA 2
+#define P_LB 3
+#define P_LP 4
+#define P_LO 5
 
 #define UNUSED(x) (void)(x)
 
@@ -50,10 +54,55 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #define MDL_CHECK_PARAMETERS
 #if defined(MDL_CHECK_PARAMETERS) && defined(MATLAB_MEX_FILE)
+
+/**
+ * \brief Check the s-function parameters for a real joystick
+ * \param[in] S Simulink structure.
+ */
+void mdlCheckParameters_REALJoy( SimStruct *S )
+{
+  int32_T locKey = int32_T( mxGetScalar( ssGetSFcnParam( S, P_JOYID ) ) );
+  Joystick myJoy;
+  if( !myJoy.Initialise( locKey ) )
+  {
+    ssSetErrorStatus( S, "sfun-osx-joystick::mdlCheckParameters Selected Joystick is not available.");
+    return;
+  }
+}
+
+/**
+ * \brief Check the s-function parameters for a dummy (NULL) joystick
+ * \param[in] S Simulink structure.
+ */
+void mdlCheckParameters_NULLJoy( SimStruct *S )
+{
+  if( !IS_PARAM_DOUBLE( ssGetSFcnParam( S, P_LA ) ) )
+  {
+    ssSetErrorStatus( S, "sfun_osx_joystick::mdlCheckParameters lA (any axes?) must be a scalar double.");
+    return;
+  }
+  if( !IS_PARAM_DOUBLE( ssGetSFcnParam( S, P_LB ) ) )
+  {
+    ssSetErrorStatus( S, "sfun_osx_joystick::mdlCheckParameters lB (any buttons?) must be a scalar double.");
+    return;
+  }
+  if( !IS_PARAM_DOUBLE( ssGetSFcnParam( S, P_LP ) ) )
+  {
+    ssSetErrorStatus( S, "sfun_osx_joystick::mdlCheckParameters lP (any POVs?) must be a scalar double.");
+    return;
+  }
+  if( !IS_PARAM_DOUBLE( ssGetSFcnParam( S, P_LO ) ) )
+  {
+    ssSetErrorStatus( S, "sfun_osx_joystick::mdlCheckParameters lO (any outputs?) must be a scalar double.");
+    return;
+  }
+}
+
 /**
  * \brief Check s-function parameters
+ * \param[in] S Simulink structure.
  */
-static void mdlCheckParameters(SimStruct *S)
+static void mdlCheckParameters( SimStruct *S )
 {
   // Make sure we have the correct number of parameters
   int_T numParams = ssGetSFcnParamsCount(S);
@@ -64,34 +113,27 @@ static void mdlCheckParameters(SimStruct *S)
     ssSetErrorStatus( S, msg );
     return;
   }
-  const mxArray *pVal;
   
   // Make sure that the Joystick LocationKey is an int32
-  pVal = ssGetSFcnParam( S, P_JOYID );
-  if ( !IS_PARAM_INT32( pVal ) )
+  if ( !IS_PARAM_INT32( ssGetSFcnParam( S, P_JOYID ) ) )
   {
     ssSetErrorStatus(S, "sfun-osx-joystick::mdlCheckParameters Joystick ID must be an int32.");
     return;
   }
   
   // Make sure the appropriate device is available.
-  int32_t locKey = int32_t( mxGetScalar( pVal ) );
-  Joystick myJoy;
-  if( !myJoy.Initialise( locKey ) )
-  {
-    ssSetErrorStatus( S, "sfun-osx-joystick::mdlCheckParameters Selected Joystick is not available.");
-    return;
-  }
+  int32_t locKey = int32_t( mxGetScalar( ssGetSFcnParam( S, P_JOYID ) ) );
+  if( locKey == 0 ) mdlCheckParameters_NULLJoy( S );
+  else mdlCheckParameters_REALJoy( S );
   
   // Make sure the sampling time is a scalar double.
-  pVal = ssGetSFcnParam( S, P_TS );
-  if ( !IS_PARAM_DOUBLE( pVal ) )
+  if ( !IS_PARAM_DOUBLE( ssGetSFcnParam( S, P_TS ) ) )
   {
     ssSetErrorStatus( S, "sfun-osx-joystick::mdlCheckParameters Sample time must be a double scalar." );
     return;
   }
   // Make sure the sampling time is positive
-  real_T Ts = mxGetScalar( pVal );
+  real_T Ts = mxGetScalar( ssGetSFcnParam( S, P_TS ) );
   if( (Ts < 0.0) & (Ts != -1.0)  )
   {
     ssSetErrorStatus( S, "sfun-osx-joystick::mdlCheckParameters Sample time must be positive, or -1 for inherited." );
@@ -101,11 +143,10 @@ static void mdlCheckParameters(SimStruct *S)
 #endif
 
 /**
- * \brief Provide size information to Simulink.
- *
+ * \brief Provide size information to Simulink about the real Joystick.
  * \param[in,out] S Simulink structure.
  */
-static void mdlInitializeSizes(SimStruct *S)
+void mdlInitializeSizes_REALJoy( SimStruct *S )
 {
   // Initialise the joystick, so we can retrieve its IO capabilities
   Joystick myJoy;
@@ -117,19 +158,6 @@ static void mdlInitializeSizes(SimStruct *S)
   }
   // Retrieve IO capability information from the joystick.
   vector<int> JoyIO = myJoy.QueryIO();
-
-  // Number of expected parameters, and check them.
-  ssSetNumSFcnParams( S, NUM_PARAMS );
-  mdlCheckParameters(S);
-  if (ssGetErrorStatus(S) != NULL) return;
-  
-  // No tunable parameters.
-  ssSetSFcnParamTunable( S, P_JOYID, SS_PRM_NOT_TUNABLE );
-  ssSetSFcnParamTunable( S, P_TS, SS_PRM_NOT_TUNABLE );
-
-  // No internal dynamic states
-  ssSetNumContStates(S, 0);
-  ssSetNumDiscStates(S, 0);
   
   // Set the number of input ports to the number of Outputs to the Joystick
   bool result;
@@ -137,6 +165,7 @@ static void mdlInitializeSizes(SimStruct *S)
   {
     result = ssSetNumInputPorts( S, 1 );
     ssSetInputPortWidth( S, 0, JoyIO[ kJoystick_Outputs ] );
+    ssSetInputPortDataType( S, 0, SS_DOUBLE );
   }
   else result = ssSetNumInputPorts( S, 0 );
   if( !result )
@@ -180,7 +209,123 @@ static void mdlInitializeSizes(SimStruct *S)
     ssSetOutputPortDataType( S, jj, SS_DOUBLE );
     jj++;
   }
+}
 
+/**
+ * \brief Provide size information to Simulink about the dummy (NULL) Joystick.
+ * \param[in,out] S Simulink structure.
+ */
+void mdlInitializeSizes_NULLJoy( SimStruct *S )
+{
+  // Get 'logical' values as to whether to enable inputs and outputs
+  int lA, lB, lP, lO;
+  lA = int( mxGetScalar( ssGetSFcnParam( S, P_LA ) ) );
+  lB = int( mxGetScalar( ssGetSFcnParam( S, P_LB ) ) );
+  lP = int( mxGetScalar( ssGetSFcnParam( S, P_LP ) ) );
+  lO = int( mxGetScalar( ssGetSFcnParam( S, P_LO ) ) );
+  
+  // Set the number of input ports and the input data size
+  bool result;
+  if( lO )
+  {
+    result = ssSetNumInputPorts( S, 1 );
+    ssSetInputPortWidth( S, 0, DYNAMICALLY_SIZED );
+    ssSetInputPortDataType( S, 0, SS_DOUBLE );
+  }
+  else result = ssSetNumInputPorts( S, 0 );
+  if( result )
+  {
+    ssSetErrorStatus( S, "sfun-osx-joystick::mdlInitializeSizes Failed to set the number of input ports.\n" );
+    return;
+  }
+  
+  // Set the number of output ports
+  int numOutputs = 0;
+  if( lA ) numOutputs++;
+  if( lB ) numOutputs++;
+  if( lP ) numOutputs++;
+  
+  result = ssSetNumOutputPorts( S, numOutputs );
+  if( result )
+  {
+    ssSetErrorStatus( S, "sfun-osx-joystick::mdlInitializeSizes Failed to set the number of output ports.\n");
+    return;
+  }
+  
+  // Set the output port sizes and data types
+  int output = 0;
+  if( lA )
+  {
+    ssSetOutputPortWidth( S, output, DYNAMICALLY_SIZED );
+    ssSetOutputPortDataType( S, output, SS_DOUBLE );
+    output++;
+  }
+  if( lB )
+  {
+    ssSetOutputPortWidth( S, output, DYNAMICALLY_SIZED );
+    ssSetOutputPortDataType( S, output, SS_BOOLEAN );
+    output++;
+  }
+  if( lP )
+  {
+    ssSetOutputPortWidth( S, output, DYNAMICALLY_SIZED );
+    ssSetOutputPortDataType( S, output, SS_DOUBLE );
+    output++;
+  }
+}
+
+#define MDL_SET_INPUT_PORT_WIDTH
+/**
+ * \brief Dynamically set the input port width.
+ */
+static void mdlSetInputPortWidth( SimStruct *S, int_T port, int_T inputPortWidth )
+{
+  ssSetInputPortWidth( S, port, inputPortWidth );
+}
+
+#define MDL_SET_OUTPUT_PORT_WIDTH
+/**
+ * \brief Dynamically set the output port width.
+ */
+static void mdlSetOutputPortWidth( SimStruct *S, int_T port, int_T outputPortWidth )
+{
+  ssSetOutputPortWidth(S,port,outputPortWidth);
+}
+
+# define MDL_SET_DEFAULT_PORT_DIMENSION_INFO
+/**
+ * \brief Set the default port sizes to be 1.
+ */
+static void mdlSetDefaultPortDimensionInfo( SimStruct *S )
+{
+  // Get 'logical' values as to whether to enable inputs and outputs
+  int output = 0;
+  if( mxGetScalar( ssGetSFcnParam( S, P_LA ) ) > 0 ) ssSetOutputPortWidth( S, output++, 1 );
+  if( mxGetScalar( ssGetSFcnParam( S, P_LB ) ) > 0 ) ssSetOutputPortWidth( S, output++, 1 );
+  if( mxGetScalar( ssGetSFcnParam( S, P_LP ) ) > 0 ) ssSetOutputPortWidth( S, output++, 1 );
+  if( mxGetScalar( ssGetSFcnParam( S, P_LO ) ) > 0 ) ssSetInputPortWidth( S, 0, 1 );
+}
+
+/**
+ * \brief Provide size information to Simulink.
+ *
+ * \param[in,out] S Simulink structure.
+ */
+static void mdlInitializeSizes(SimStruct *S)
+{
+  // Number of expected parameters, and check them.
+  ssSetNumSFcnParams( S, NUM_PARAMS );
+  mdlCheckParameters(S);
+  if (ssGetErrorStatus(S) != NULL) return;
+  
+  // No tunable parameters.
+  ssSetSFcnParamTunable( S, P_JOYID, SS_PRM_NOT_TUNABLE );
+  ssSetSFcnParamTunable( S, P_TS, SS_PRM_NOT_TUNABLE );
+
+  // No internal dynamic states
+  ssSetNumContStates(S, 0);
+  ssSetNumDiscStates(S, 0);
+  
   // Number of sample times (1)
   ssSetNumSampleTimes(S, 1);
   // No Real work vector
@@ -197,6 +342,10 @@ static void mdlInitializeSizes(SimStruct *S)
   ssSetSimStateCompliance( S, USE_DEFAULT_SIM_STATE );
   // No options set.
   ssSetOptions(S, 0);
+
+  int32_t locKey = int32_t( mxGetScalar( ssGetSFcnParam( S, P_JOYID ) ) );
+  if( locKey == 0 ) mdlInitializeSizes_NULLJoy( S );
+  else mdlInitializeSizes_REALJoy( S );
 }
 
 
@@ -214,13 +363,19 @@ static void mdlInitializeSampleTimes(SimStruct *S)
   ssSetModelReferenceSampleTimeDefaultInheritance(S);
 }
 
-#define MDL_START
 /**
- * \brief At the start of model execution, initialise the Joystick.
- *
- * \param[in,out] S Simulink structure.
+ * \brief Start the dummy (NULL) joystick block.
  */
-static void mdlStart(SimStruct *S)
+void mdlStart_NULLJoy( SimStruct *S )
+{
+  ssGetPWork(S)[0] = NULL;
+  ssGetPWork(S)[1] = NULL;
+}
+
+/**
+ * \brief Start the real joystick block
+ */
+void mdlStart_REALJoy( SimStruct *S )
 {
   // Create a new Joystick object, and get the pointer to it.
   Joystick *myJoy = new Joystick;
@@ -274,13 +429,36 @@ static void mdlStart(SimStruct *S)
   ssGetPWork(S)[1] = (vector<int> *) &JoyIO;
 }
 
+#define MDL_START
 /**
- * \brief Read the Joystick states and output them.
+ * \brief At the start of model execution, initialise the Joystick.
+ *
+ * \param[in,out] S Simulink structure.
  */
-static void mdlOutputs(SimStruct *S, int_T tid)
+static void mdlStart( SimStruct *S )
+{
+  int devId = mxGetScalar( ssGetSFcnParam( S, P_JOYID ) );
+  if( devId==0 ) mdlStart_NULLJoy( S );
+  else mdlStart_REALJoy( S );
+}
+
+
+/**
+ * \brief Outputs (and inputs) to the dummy (NULL) block.
+ */
+void mdlOutputs_NULLJoy( SimStruct *S, int_T tid )
+{
+  UNUSED( S );
+  UNUSED( tid );
+}
+
+/**
+ * \brief Outputs from (and output to) the real joystick
+ */
+void mdlOutputs_REALJoy( SimStruct *S, int_T tid )
 {
   UNUSED( tid );
-  Joystick *myJoy = (Joystick *) ssGetPWork(S)[0];
+    Joystick *myJoy = (Joystick *) ssGetPWork(S)[0];
   vector<int> *JoyIO = (vector<int> *) ssGetPWork(S)[1];
   
   // Poll the Joystick axes
@@ -338,7 +516,16 @@ static void mdlOutputs(SimStruct *S, int_T tid)
     }
     copy( pr, pr+outputs.size(), outputs.begin() );
   }
-  
+}
+
+/**
+ * \brief Read the Joystick states and output them.
+ */
+static void mdlOutputs(SimStruct *S, int_T tid)
+{
+  int JoyLocKey = mxGetScalar( ssGetSFcnParam( S, P_JOYID ) );
+  if( JoyLocKey == 0 ) mdlOutputs_NULLJoy( S, tid );
+  else mdlOutputs_REALJoy( S, tid );
 }                                                
 
 /**
@@ -348,9 +535,13 @@ static void mdlOutputs(SimStruct *S, int_T tid)
  */
 static void mdlTerminate(SimStruct *S)
 {
-  // Retrieve and delete the Joystick object.
-  Joystick *myJoy =  (Joystick *) ssGetPWork(S)[0];
-  delete myJoy;
+  int JoyLocKey = mxGetScalar( ssGetSFcnParam( S, P_JOYID ) );
+  if( JoyLocKey != 0 )
+  {
+    // Retrieve and delete the Joystick object.
+    Joystick *myJoy =  (Joystick *) ssGetPWork(S)[0];
+    delete myJoy;
+  }
 }
 
 // Required s-function trailer
